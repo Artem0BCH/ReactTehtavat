bad155ac3b1999ea4d2365aa91332496de723e1d
Artem Olshanchenko <S2401214@edu.bc.fi>
2026-05-06 10:35:37 +0300

    Counter tehtävät jatkuu

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,15 +1,35 @@
-import * as React from "react";
 import { useState } from "react";
+
+function LaskuriNaytto({ count }) {
+  return <p>{count}</p>;
+}
+
+function KasvatusPainike({ onClick }) {
+  return <button onClick={onClick}>Kasvata</button>;
+}
+
+function VahennysPainike({ onClick }) {
+  return <button onClick={onClick}>Vähennä</button>;
+}
+
+function NollausPainike({ onClick }) {
+  return <button onClick={onClick}>Nollaa</button>;
+}
+
+function TuplaKasvatus({ onClick }) {
+  return <button onClick={onClick}>TuplaKasvatus</button>;
+}
 
 function Laskuri() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Kasvata</button>
-      <button onClick={() => setCount(count - 1)}>Vähennä</button>
-      <button onClick={() => setCount(0)}>Nollaa</button>
+      <LaskuriNaytto count={count} />
+      <KasvatusPainike onClick={() => setCount(count + 1)} />
+      <VahennysPainike onClick={() => setCount(count - 1)} />
+      <NollausPainike onClick={() => setCount(0)} />
+      <TuplaKasvatus onClick={() => setCount(count + 2)} />
     </div>
   );
 }
